7cd7d35f14af8323860f3864ff372dc8b6e8af39
Robin Dillenburg <tonitornado@gmail.com>
2020-11-03 21:38:39 +0100

    simple counter example

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverbloc/riverbloc.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() => emit(state + 1);
+}
+
+final counterCubitProvider = BlocProvider((ref) => CounterCubit());
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(),
+      );
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    final counter = watch(counterCubitProvider.state);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Welcome'),
+        title: const Text('Welcome'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () => context.refresh(counterCubitProvider),
+          ),
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Let\'s explore Riverpod with Cubits!',
-            ),
+            const Text('Cast your vote for Flutter!'),
+            const SizedBox(height: 30),
+            Text('Votes: $counter', style: Theme.of(context).textTheme.headline5),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Action Button',
-        child: Icon(Icons.refresh),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () => context.read(counterCubitProvider).increment(),
+        tooltip: 'Vote for Flutter',
+        child: const Icon(Icons.ballot),
+      ),
     );
   }
 }
